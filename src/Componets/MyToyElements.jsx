9a5f4c91248868bcc28b2f5e5eecb29e7ddb0641
akil-tajwar <akiltajwar@gmail.com>
2023-05-23 16:16:43 +0600

    my toy delete bug fixed

--- src/Componets/MyToyElements.jsx
+++ src/Componets/MyToyElements.jsx
@@ -1,45 +1,10 @@
 import React, { useState } from 'react';
 import { RiDeleteBinLine } from 'react-icons/ri';
 import { HiOutlinePencilAlt } from 'react-icons/hi';
-import Swal from 'sweetalert2';
 
-const MyToyElements = ({ myToyElements }) => {
+const MyToyElements = ({ myToyElements, handleDelete }) => {
     const { _id, tname, sname, url, category, price, quantity } = myToyElements;
-    const [myToys, setmyToys] = useState([]);
     const [count, setCount] = useState(1);
-
-    const handleDelete = (_id) => {
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                fetch(`http://localhost:5000/mytoys/${_id}`, {
-                    method: 'DELETE'
-                })
-                    .then(res => res.json())
-                    .then(data => {
-                        console.log(data);
-                        if (data.deletedCount > 0) {
-                            Swal.fire(
-                                'Deleted!',
-                                'Your file has been deleted.',
-                                'success'
-                            )
-                            const remaining = myToys.filter(myToy => myToy._id !== _id);
-                            setmyToys(remaining);
-
-                        }
-                    })
-                    .catch(error => console.log(error))
-            }
-        })
-    }
 
     return (
         <div className='w-3/4 mx-auto'>
--- src/Componets/MyToys.jsx
+++ src/Componets/MyToys.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from './AuthProvider';
 import MyToyElements from './MyToyElements';
+import Swal from 'sweetalert2';
 
 const MyToys = () => {
     const { user } = useContext(AuthContext);
@@ -19,9 +20,42 @@ const MyToys = () => {
     if (isLoading) {
         <p>Loading...</p>
     }
+
+    const handleDelete = (_id) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/mytoys/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                            const remaining = myToys.filter(myToy => myToy._id !== _id);
+                            setMyToys(remaining);
+
+                        }
+                    })
+                    .catch(error => console.log(error))
+            }
+        })
+    }
     return (
         <div className='pt-56 pb-20'>
-            {/* <h1>{user.length}</h1> */}
+            <h1 className='text-5xl text-center font-semibold pb-5 text-[#1d7edd]'>My Toys</h1>
             <div className="border w-3/4 h-fit mx-auto border-slate-400">
                 <div className='font-semibold grid grid-cols-9 justify-center items-center p-5'>
                     <p className='font-semibold'>Sl no.</p>
@@ -35,7 +69,7 @@ const MyToys = () => {
                 </div>
             </div>
             {
-                myToys.map(myToyElements => <MyToyElements myToyElements={myToyElements} key={myToyElements._id}></MyToyElements>)
+                myToys.map(myToyElements => <MyToyElements handleDelete={handleDelete} myToyElements={myToyElements} key={myToyElements._id}></MyToyElements>)
             }
         </div>
     );
